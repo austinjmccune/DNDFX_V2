1076c1b74c532e547cd343d9092b3a06416bd0d9
austinjmccune <austinjmccune@gmail.com>
2021-02-20 21:51:10 -0500

    attempting to manage processes with multiprocessing in order to run effects on multiple spells at once.

--- main.py
+++ main.py
@@ -519,7 +519,7 @@ while True:
         origin = input('where would you like to cast it?')
         processes.append(multiprocessing.Process(target=cast,args=[spell,origin,grid,led_dict]))
         processes[i].start()
-        print(processes.count())
+        print(len(processes))
         #cast(spell,origin,grid,led_dict)
 
 
